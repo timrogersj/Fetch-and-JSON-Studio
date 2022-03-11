00e12499d213dd2c9a46997f3adfab8612ba211c
Your Name~ <timrogersj@gmail.com>
2022-03-10 20:21:56 -0600

    completed studio

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 // TODO: add code here
+
+
+window.addEventListener("load", function(response) {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+       return response.json();
+    }).then(function(json) {
+        console.log(json);
+        let container = document.getElementById("container");
+        let htmlString = "";
+        for (astronaut of json) {
+            htmlString += `
+            <div class="astronaut">
+                <div class="bio">
+                    <h3>${astronaut.firstName} ${astronaut.lastName} </h3>
+                        <ul>
+                            <li>Hours in space: ${astronaut.hoursInSpace} </li>
+                            <li>Active: ${astronaut.active} </li>
+                            <li>Skills: ${astronaut.skills} </li>
+                        </ul>
+                </div>
+                <img class="avatar" src=${astronaut.picture}>
+            </div> `
+        }
+
+
+
+
+
+        container.innerHTML = htmlString;
+    })
+});
